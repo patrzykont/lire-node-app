437a5816b9b709eb027fde474207405abf8f4194
patrzykont <aaronpatrzykont@Aarons-MacBook-Air.local>
2017-11-30 18:30:50 -0600

    didnt get to the json

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<MpEwcULcYqtnYzZsN9yumzUKd>',
+  consumer_secret: '<7P1OwKYkrjkMhgjdaRMoqZ7ur5agIl2RSoA1OGVjYWwrSJOPdP>',
+  access_token_key: '<935016900047798272-6VNhk12v2DcQn1S5fRPxFGupmNOR2so>',
+  access_token_secret: '<dJAV9cMThRogorMitV5trGKN8ofqy3vNr72Qu5x11vyf3>',
+}
+
+module.exports = twitterKeys;
